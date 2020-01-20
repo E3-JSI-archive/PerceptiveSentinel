4442200ce0d9455fa7882eafda92703d246bc323
Beno Šircelj <benosircelj@gmail.com>
2020-01-20 13:03:15 +0100

    init fix

--- Utilities/LargeDataProcessing/__init__.py
+++ Utilities/LargeDataProcessing/__init__.py
@@ -2,11 +2,5 @@
 This module lists all externally useful classes and functions
 """
 
-from .eopatches import check_country, check_year, check_file, get_bbox_gdf, get_bbox_splitter, save_bbox_gdf
-from .plots import scl_classes, scl_cmap, scl_norm, draw_bbox, draw_vector_timeless, draw_true_color
-from .plots import draw_scene_classification, draw_mask
-from .tasks import CreatePatch, AddGeopediaVectorFeature, AddAreaRatio
-from .tasks import ValidDataFractionPredicate, Sen2CorValidData, SentinelHubValidData, MergeMasks, CountValid
-from .workflows import get_create_and_add_lpis_workflow, get_add_l2a_data_workflow
-from .gvservices import GeoVilleServices
-from .utilities import get_group_id
+from .Sampling import sample_patches
+from .clustering import ClusteringTask
